d02e6b478672cb9aa723a53690048d5ac1675c4b
kubikulek231 <kuba.lepik@seznam.cz>
2023-03-07 14:43:05 +0100

    lab 5 finished

---
+++ 05-ffs/flip_flops/flip_flops.srcs/sources_1/new/top.vhd
@@ -0,0 +1,103 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/07/2023 02:19:04 PM
+-- Design Name: 
+-- Module Name: top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           BTNC : in STD_LOGIC;
+           SW : in STD_LOGIC_VECTOR (0 downto 0);
+           LED : in STD_LOGIC_VECTOR (3 downto 0));
+end top;
+
+------------------------------------------------------------------------
+-- Architecture body for top level
+------------------------------------------------------------------------
+architecture behavioral of top is
+
+  -- Internal signals between flip-flops
+  signal sig_ff0 : std_logic;
+  signal sig_ff1 : std_logic;
+  signal sig_ff2 : std_logic;
+  signal sig_ff3 : std_logic;
+  
+  signal sig_ff0_bar : std_logic;
+  signal sig_ff1_bar : std_logic;
+  signal sig_ff2_bar : std_logic;
+  signal sig_ff3_bar : std_logic;
+
+  -- WRITE YOUR CODE HERE
+
+begin
+
+  --------------------------------------------------------------------
+  -- Four instances (copies) of D-type FF entity
+  d_ff_0 : entity work.d_ff_rst
+      port map (
+          clk => CLK100MHZ,
+          rst => BTNC,
+          -- WRITE YOUR CODE HERE
+          q  => sig_ff0,
+          q_bar => sig_ff0_bar
+          
+
+      );
+
+  d_ff_1 : entity work.d_ff_rst
+      port map (
+          clk => CLK100MHZ,
+          rst => BTNC,
+          -- WRITE YOUR CODE HERE
+          q  => sig_ff1,
+          q_bar => sig_ff1_bar
+      );
+  d_ff_2 : entity work.d_ff_rst
+      port map (
+          clk => CLK100MHZ,
+          rst => BTNC,
+          -- WRITE YOUR CODE HERE
+          q  => sig_ff2,
+          q_bar => sig_ff2_bar
+
+      );
+   d_ff_3 : entity work.d_ff_rst
+      port map (
+          clk => CLK100MHZ,
+          rst => BTNC,
+          -- WRITE YOUR CODE HERE
+          q  => sig_ff3,
+          q_bar => sig_ff3_bar
+
+      );
+
+  -- PUT OTHER TWO FLIP-FLOP INSTANCES HERE
+
+end architecture behavioral;
